9ce2455031711727fbdc9a75fd69277829275332
Alpesh Rama <crealysislimited@gmail.com>
2025-08-02 18:04:29 +0100

    rolled back to dotnet 8 for compatibility with ec2

--- Program.cs
+++ Program.cs
@@ -25,14 +25,14 @@ builder.Services.AddIdentityApiEndpoints<User>(opt =>
     .AddEntityFrameworkStores<StoreContext>();
 
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+//builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    //app.MapOpenApi();
     app.MapScalarApiReference();
 }
 
